f1b63c4ab952b05551b675e31e6941f898381470
Gergy <gergy2k07@gmail.com>
2026-05-26 21:00:19 +0300

    comments

--- main.cpp
+++ main.cpp
@@ -1,86 +1,86 @@
-/* Шапка
-                                  ██████████        ██████████
-                                ██░░▒▒░░▒▒▒▒▓▓▓▓▓▓▓▓▒▒▒▒░░░░▒▒▓▓
-                                ██░░░░░░░░░░▒▒▒▒▒▒▒▒░░░░░░░░░░██
-                              ▓▓▒▒░░░░░░░░░░▒▒░░░░▒▒░░░░░░░░░░▒▒▓▓
-                              ██░░░░░░░░░░░░░░▒▒▒▒▒▒░░░░░░░░░░░░██
-                              ██░░░░░░░░░░░░▒▒░░░░▒▒░░░░░░░░░░░░██
-                              ██░░░░░░░░░░▒▒▒▒░░░░▒▒░░░░░░░░░░░░██
-                              ██░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░██
-                              ██░░▒▒░░░░░░░░▒▒░░░░▒▒▒▒░░░░▒▒░░▒▒██
-                              ██░░░░░░░░░░░░░░▒▒▒▒░░░░░░░░░░░░░░██
-                              ██░░░░░░░░░░░░▒▒▒▒▒▒▒▒░░░░░░░░░░░░██
-  ██████                      ██░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░██                      ██████
-██░░░░░░██                  ██░░░░░░░░░░░░▒▒░░░░░░░░░░░░░░░░░░░░░░██                  ██▒▒░░░░██
-██▒▒░░░░░░██                ██░░░░░░░░░░░░░░▒▒░░▒▒▒▒░░░░░░░░░░░░░░██                ██░░░░░░▒▒██
-██░░░░░░░░░░████            ██░░░░░░░░░░░░░░░░▒▒▒▒░░░░░░░░░░░░░░░░██            ████░░░░░░░░░░██
-██░░░░░░░░▒▒░░░░████████████░░░░░░░░░░░░░░░░▒▒░░░░▒▒░░░░░░░░░░░░░░░░████████████░░░░░░░░░░░░░░██
-██▒▒░░░░░░░░░░░░▒▒░░▒▒▒▒▒▒██░░░░░░░░░░░░░░░░░░░░░░▒▒░░░░░░░░░░░░░░░░██▒▒▒▒▒▒░░░░░░░░░░░░░░░░▒▒██
-██░░░░░░░░░░░░░░░░░░░░░░████░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░████░░░░░░░░░░░░░░░░░░░░░░██
-  ██░░░░░░░░░░░░░░░░░░░░██▒▒██░░░░░░░░░░░░░░▒▒░░░░▒▒░░░░░░░░░░░░░░██▒▒██░░░░░░░░░░░░░░░░░░░░██
-  ██▒▒░░░░░░░░░░░░░░░░░░██▒▒░░████░░░░░░░░░░░░▒▒▒▒░░░░░░░░░░░░████░░▒▒██░░░░░░░░░░░░░░░░░░▒▒██
-  ██▒▒░░░░░░░░░░░░░░░░░░██▒▒░░▒▒▒▒██████░░░░▒▒░░▒▒▒▒░░░░██████░░▒▒░░▒▒██░░░░░░░░░░░░░░░░░░▒▒██
-    ██▒▒░░░░▒▒░░░░░░░░░░░░██░░▒▒░░▒▒▒▒░░████████████████░░▒▒▒▒░░▒▒░░██░░░░░░░░░░░░░░░░░░▒▒██
-    ██▒▒░░░░░░░░░░░░░░░░░░▒▒████░░▒▒▒▒░░▓▓▒▒▒▒▓▓▓▓░░▓▓▓▓░░▒▒▒▒░░▓▓▓▓▒▒░░░░░░░░░░░░░░░░░░░░██
-      ▓▓░░▒▒░░░░░░░░░░░░░░░░▒▒▒▒██▓▓▓▓░░▒▒▒▒░░▒▒▒▒░░▒▒▒▒░░██▓▓▓▓▒▒▒▒░░░░░░░░░░░░░░░░▒▒░░▓▓
-        ██░░░░░░░░░░░░░░░░░░░░░░░░░░▒▒████████████████████░░░░░░░░░░░░░░░░░░░░░░░░░░░░██
-          ██▒▒▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▒▒░░██
-            ██░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▒▒██
-              ████░░▒▒▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▒▒░░████
-                  ████░░░░▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▒▒░░░░████
-                      ██████░░▒▒▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░▒▒░░░░██████
-                            ████████░░▒▒░░░░▒▒░░░░▒▒░░░░▒▒░░████▓▓██
-                                    ████████████████████████
+/******************************************************************************
+*                      КАФЕДРА №304 1 КУРС ПРОГИНЖ                            *
+*                           Летняя Практика                                   *
+*-----------------------------------------------------------------------------*
+* Project Type  : Win32 Console Application                                   *
+* Project Name  : MaiPractice                                                 *
+* File Name     : main.cpp                                                    *
+* Language      : C/C++                                                       *
+* Programmer    : Георгий Алтынов                                             *
+* Modified By   :                                                             *
+* Created       : 5/05/26                                                     *
+* Last Revision : 26/05/26                                                    *
+* Comment(s)    : Работа со структурами и индексной сортировкой               *
+******************************************************************************/
 
-*/
+
 #include <iostream>
 #include <fstream>
 #include <iomanip>
 #ifdef _WIN32
+// Доступ к win api для смены кодировки консоли, если программа собирается из-под windows
 #include <windows.h>
 #endif
 
-#include "read_write.h"
+#include "read_write.h" // Реализация чтения и вспомогательных функций для вывода
 
 
-auto fname = "input.txt";
+/****************************************************************/
+/*             О С Н О В Н А Я     П Р О Г Р А М М А            */
+/****************************************************************/
 
+
+auto fname = "input.txt"; // Имя входного файла
+
+// Индексная сортировка массива данных пузырьком
 void bubble_index_sort(const Line *data, size_t shape, size_t *indexes);
 
+// Вывод таблицы
 void compile_table(const Line *data, size_t shape, const size_t *indexes);
 
 
 int main() {
 #ifdef _WIN32
+    // Если программа собирается из-под windows, меняем кодировку консоли на UTF-8
+    // В Linux и Darwin она выставлена по умолчанию
     SetConsoleOutputCP(CP_UTF8);
 #endif
 
-    std::ifstream input(fname);
+    std::ifstream input(fname); // Файловый поток для чтения
     if (!input.is_open()) {
+        // Обработка ошибки открытия файла
         std::cerr << "Failed to open file: `" << fname << "`" << std::endl;
         return -1;
     }
 
-    constexpr size_t buffsize = 512;
-    constexpr size_t df_size = 1000;
-    char buffer[buffsize];
-    Line data[df_size];
-    size_t good_rows = 0;
-    size_t all_rows = 0;
-    bool bad_data = false;
+    constexpr size_t buffsize = 512; // Размер буфера для чтения текущей строки
+    constexpr size_t df_size = 1000; // Общий размер массива данных
+    char buffer[buffsize]; // Буфер для текущей строки
+    Line data[df_size]; // Массив данных АСУ ВД
+    size_t indexes[df_size]; // Массив индексов для индексной сортировки
+    size_t good_rows = 0; // Количество корректных строк
+    size_t all_rows = 0; // Общее количество строк
+    bool bad_data = false; // Флаг вывода шапки с некорректными строками
 
+    // Чтение строк до ошибки потока (EOF или переполнение буфера)
     while (input.getline(buffer, buffsize)) {
-        const size_t line_size = strlen(buffer);
+        const size_t line_size = strlen(buffer); // Подсчет длины считанной строки
         if (ErrCode err = parse_line(buffer, data[good_rows]);
             err != GOOD || (err = check_logical(data, good_rows)) != GOOD) {
+            // Получаем код ошибки от функции парсинга строки
+            // Если он GOOD, берем код от функции поиска логических ошибок. Если он тоже GOOD - строка корректна
+            // Иначе печатаем информацию об ошибке
+
             ++all_rows;
             if (!bad_data) {
+                // Вывод шапки, если ее не было
                 bad_data = true;
                 std::cout << repeat{"=", 45} << std::endl;
                 std::cout << "Bad lines:" << std::endl;
                 std::cout << repeat{"=", 45} << std::endl << std::endl;
             }
-            restore(buffer, line_size);
+            restore(buffer, line_size); // Восстановление строки перед печатью
+            // Печать строки и информации об ошибке в ней
             std::cout << "#" << all_rows << " " << buffer << " ─ " << std::endl << err << std::endl << std::endl;
             continue;
         }
@@ -88,10 +88,12 @@ int main() {
         ++all_rows;
         ++good_rows;
         if (good_rows == df_size) {
+            // Если вышли за предел размера массива, заканчиваем чтение
             std::cerr << "DataFrame size limit exceeded. End parsing" << std::endl;
             break;
         }
     }
+    // Вывод шапки с информацией о количестве считанных строк
     std::cout << repeat{"=", 45} << std::endl;
     std::cout << "Total parsed: " << good_rows << "/" << all_rows << std::endl;
     if (good_rows == 0) {
@@ -100,23 +102,30 @@ int main() {
     }
     std::cout << repeat{"=", 45} << std::endl << std::endl;
 
-    size_t indexes[df_size];
+    // Инициализация массива индексов
     for (size_t i = 0; i < good_rows; i++) {
         indexes[i] = i;
     }
 
+    // Индексная сортировка пузырьком
     bubble_index_sort(data, good_rows, indexes);
 
+    // Вывод таблицы с отсортированными данными
     std::cout << "Data table sorted by flight number:" << std::endl;
     compile_table(data, good_rows, indexes);
 }
 
 
+// Индексная сортировка массива данных пузырьком
 void bubble_index_sort(const Line *data, const size_t shape, size_t *indexes) {
+    // Сортируется массив индексов, массив структур не изменяется
+
     for (size_t i = 0; i < shape - 1; i++) {
         bool has_swp = false;
         for (size_t j = 0; j < shape - i - 1; j++) {
+            // Сравнение бортовых номеров структур под номерами j и j+1
             if (data[indexes[j]].flight_number > data[indexes[j + 1]].flight_number) {
+                // Местами меняются сами номера j и j+1
                 has_swp = true;
                 const size_t buff = indexes[j];
                 indexes[j] = indexes[j + 1];
@@ -128,25 +137,33 @@ void bubble_index_sort(const Line *data, const size_t shape, size_t *indexes) {
 }
 
 
+// Вывод таблицы
 void compile_table(const Line *data, const size_t shape, const size_t *indexes) {
+    // Шапка таблицы
     std::cout << "┌" << repeat{"─", 3} << "┬" << repeat{"─", 6} << "┬" << repeat{"─", 8} << "┬" <<
             repeat{"─", 17} << "┬" << repeat{"─", 5} << "┐" << std::endl;
     std::cout << "│" << std::setw(3) << "#" << "│" << std::setw(6) << "Bort" << "│" << std::setw(8)
             << "Flight" << "│" << std::setw(17) << "Aircraft" << "│" << std::setw(5) << "Time" << "│" << std::endl;
 
     for (size_t i = 0; i < shape; i++) {
+        // i-ая строка таблицы
         const auto [bort_number, flight_number, aircraft, arrival] = data[indexes[i]];
         std::cout << "├" << repeat{"─", 3} << "┼" << repeat{"─", 6}
                 << "┼" << repeat{"─", 8} << "┼" << repeat{"─", 17} << "┼"
                 << repeat{"─", 5} << "┤" << std::endl;
+        // Расчет выравнивания для марки ЛА с учетом особенностей UTF-8
         const int align = 17 + static_cast<int>(strlen(aircraft) - utf8_length(aircraft));
 
+        // Вывод строки
         std::cout << std::setfill(' ') << "│" << std::setw(3) << i << "│" << bort_prefix << std::setfill('0') <<
                 std::setw(4) << bort_number << "│" << flight_prefix << std::setw(4) << flight_number << "│" <<
                 std::setfill(' ') << std::setw(align) << aircraft << "│" << std::setfill('0') << std::setw(2) <<
                 arrival.hours << ":" << std::setw(2) << arrival.minutes << "│" << std::endl;
     }
 
+    // Конец таблицы
     std::cout << "└" << repeat{"─", 3} << "┴" << repeat{"─", 6} << "┴" << repeat{"─", 8}
             << "┴" << repeat{"─", 17} << "┴" << repeat{"─", 5} << "┘" << std::endl;
 }
+
+/* -------------    END OF FILE main.cpp    ------------- */
--- read_write.h
+++ read_write.h
@@ -2,52 +2,78 @@
 
 #include <cstring>
 
-constexpr size_t MAX_NAME_LENGTH = 18;
 
-inline auto bort_prefix = "Б-";
-inline auto flight_prefix = "РЕЙС";
+constexpr size_t MAX_NAME_LENGTH = 18; // Максимальный размер названия модели самолёта
+
+inline auto bort_prefix = "Б-"; // Начало бортового номера
+inline auto flight_prefix = "РЕЙС"; // Начало номера рейса
 
 
+// Коды ошибок
 enum ErrCode {
-    ERR_BNUM_VAL, ERR_FLIGHT_VAL, ERR_NAME_LENGTH, ERR_TIME_VAL, ERR_TOK_COUNT,
-    ERR_MODELS_MISMATCH, ERR_SAME_TIME, GOOD
+    ERR_BNUM_VAL, // Некорректный бортовой номер
+    ERR_FLIGHT_VAL, // Некорректный номера рейса
+    ERR_NAME_LENGTH, // Слишком длинное имя модели
+    ERR_TIME_VAL, // Некорректное время
+    ERR_TOK_COUNT, // Некорректное число токенов в строке (не 4 - борт номер; номер рейса; модель; время)
+    ERR_MODELS_MISMATCH, // Логическая ошибка несоответствия моделей под одним борт номером
+    ERR_SAME_TIME, // Логическая ошибка вылета одного номера дважды за одно время
+    GOOD // Нет ошибки
 };
 
+// Структура для хранения времени
 typedef struct Time {
     unsigned short hours;
     unsigned short minutes;
 } Time;
 
+// Структура для хранения записи АСУ ВД
 typedef struct Line {
-    unsigned int bort_number;
-    unsigned int flight_number;
-    char aircraft[MAX_NAME_LENGTH];
-    Time arrival;
+    unsigned int bort_number; // Бортовой номер
+    unsigned int flight_number; // Номер рейса
+    char aircraft[MAX_NAME_LENGTH]; // Модель ЛА
+    Time arrival; // Время захода в зону
 } Line;
 
+// Структура для вывода строки str count раз
 typedef struct {
     const char *str;
     int count;
 } repeat;
 
 
+// Вывод строки структуры repeat count раз (замена std::setfill для псевдографики)
 std::ostream &operator<<(std::ostream &os, const repeat &r);
 
+// Вывод ошибки по коду
 std::ostream &operator<<(std::ostream &os, ErrCode err);
 
+// Подсчет длины последовательности Unicode символов с учётом их многобайтовости
 size_t utf8_length(const char *str);
 
+// Обработка времени
 bool parse_time(char *str, Time &obj);
 
+// Обработка числа с префиксом (bort_prefix и flight_prefix)
 int parse_with_prefix(const char *prefix, const char *str);
 
+// Парсинг неотрицательного целого числа из строки
 int strtoi(const char *str);
 
+// Проверка равенства времени
+bool time_cmp(const Time &a, const Time &b);
+
+// Восстановление строки после обработки strtok (убирает лишние \0)
 void restore(char *str, size_t size);
 
+// Проверка данных на логические ошибки
+ErrCode check_logical(const Line *data, size_t size);
+
+// Обработка строки данных
 ErrCode parse_line(char *str, Line &row);
 
 
+// Вывод строки структуры repeat count раз (замена std::setfill для псевдографики)
 inline std::ostream &operator<<(std::ostream &os, const repeat &r) {
     for (int i = 0; i < r.count; ++i) {
         os << r.str;
@@ -56,6 +82,7 @@ inline std::ostream &operator<<(std::ostream &os, const repeat &r) {
 }
 
 
+// Вывод ошибки по коду
 inline std::ostream &operator<<(std::ostream &os, const ErrCode err) {
     switch (err) {
         case ERR_BNUM_VAL:
@@ -68,7 +95,7 @@ inline std::ostream &operator<<(std::ostream &os, const ErrCode err) {
             os << "Модель самолета превышает допустимую длину";
             break;
         case ERR_TIME_VAL:
-            os << "Время прибытия не соответствует шаблону HH:MM";
+            os << "Время прибытия не соответствует шаблону HH:MM и H:MM";
             break;
         case ERR_TOK_COUNT:
             os << "В строке некорректное количество данных (ожидается 4 столбца)";
@@ -90,7 +117,14 @@ inline std::ostream &operator<<(std::ostream &os, const ErrCode err) {
 }
 
 
+// Подсчет длины последовательности Unicode символов с учётом их многобайтовости
 inline size_t utf8_length(const char *str) {
+    /*
+    * 0xxxxxxx для однобайтового символа, 110xxxxx для 2, 1110xxxx для 3, 11110xxx для 4.
+    * При этом второй и последующий байты всегда имеют вид 10xxxxxx
+    * Функция выявляет такие биты с помощью маски 11000000 - не первый байт даст 10000000
+    * (первые два справа разряда совпадут с интересующим байтом, остальные нули)
+    */
     size_t length = 0;
     for (const char *c = str; *c != '\0'; c++) {
         if ((*c & 0b1100'0000) != 0b1000'0000) {
@@ -101,12 +135,14 @@ inline size_t utf8_length(const char *str) {
 }
 
 
+// Парсинг неотрицательного целого числа из строки
 inline int strtoi(const char *str) {
     int res = 0;
     for (int i = 0; str[i] != '\0'; i++) {
         if (str[i] >= '0' && str[i] <= '9') {
             res = res * 10 + (str[i] - '0');
         } else {
+            // Возврат -1, если встретился символ не из 0-9, так как для корректного числа функция вернет res >= 0
             return -1;
         }
     }
@@ -114,34 +150,45 @@ inline int strtoi(const char *str) {
 }
 
 
+// Обработка времени
 inline bool parse_time(char *str, Time &obj) {
     const size_t length = strlen(str);
     if (length != 4 && length != 5) {
+        // Время может быть длины 4, если записано в формате H:MM, или 5 в формате HH:MM. Оба формата корректны
         return false;
     }
 
+    // Двоеточие всегда идет перед MM, то есть length - 3
     const size_t delim_pos = length - 3;
+    // Если не встретилось двоеточие на ожидаемом месте - ошибка
     if (str[delim_pos] != ':') return false;
 
-    str[delim_pos] = '\0';
-    const int hours = strtoi(str);
-    const int minutes = strtoi(str + delim_pos + 1);
-    str[delim_pos] = ':';
+    str[delim_pos] = '\0'; // Ставим временный разделитель между минутами и часами
+    const int hours = strtoi(str); // Парсинг минут
+    const int minutes = strtoi(str + delim_pos + 1); // Парсинг часов
+    str[delim_pos] = ':'; // Возврат ':' на место
+    // Проверка на корректность считанного времени (или -1, если парсинг не удался)
     if (hours > 23 || minutes > 59 || hours == -1 || minutes == -1) return false;
 
+    // Инициализация полей структуры
     obj.hours = hours;
     obj.minutes = minutes;
     return true;
 }
 
 
+// Обработка числа с префиксом (bort_prefix и flight_prefix)
 inline int parse_with_prefix(const char *prefix, const char *str) {
+    // Поиск нужного префикса в начале строки. Не находим - ошибка
     if (strstr(str, prefix) != str) return -1;
+    // Число будет лежать сдвинутым на префикс
     return strtoi(str + strlen(prefix));
 }
 
 
+// Восстановление строки после обработки strtok (убирает лишние \0)
 inline void restore(char *str, const size_t size) {
+    // Заменяем все \0 обратно на пробел кроме последнего (после size)
     for (int i = 0; i < size; i++) {
         if (str[i] == '\0') {
             str[i] = ' ';
@@ -150,44 +197,57 @@ inline void restore(char *str, const size_t size) {
 }
 
 
+// Проверка равенства времени
 inline bool time_cmp(const Time &a, const Time &b) {
+    // Время А равно времени Б тогда и только тогда, когда равны между собой их часы и минуты
     return a.hours == b.hours && a.minutes == b.minutes;
 }
 
 
+// Проверка данных на логические ошибки
 inline ErrCode check_logical(const Line *data, const size_t size) {
-    Line cur = data[size];
+    Line cur = data[size]; // Берем строку с конца массива и делаем проверки с каждой предыдущей
     for (const Line *line = data; line < data + size; line++) {
+        // Логическая ошибка несоответствия моделей под одним борт номером
         if (line->bort_number == cur.bort_number && strcmp(line->aircraft, cur.aircraft) != 0) {
             return ERR_MODELS_MISMATCH;
         }
+        // Логическая ошибка вылета одного борт номера дважды за одно время
         if (line->bort_number == cur.bort_number && time_cmp(line->arrival, cur.arrival)) {
             return ERR_SAME_TIME;
         }
+        // Логическая ошибка вылета одного номера рейса дважды за одно время
         if (line->flight_number == cur.flight_number && time_cmp(line->arrival, cur.arrival)) {
             return ERR_SAME_TIME;
         }
     }
+    // Если нет ошибок, вернем GOOD
     return GOOD;
 }
 
 
+// Обработка строки данных
 inline ErrCode parse_line(char *str, Line &row) {
-    const auto delim = " \t";
+    const auto delim = " \t"; // Разделители между токенами - пробел и таб
 
-    const char *tok_bnum = strtok(str, delim);
+    const char *tok_bnum = strtok(str, delim); // Токен бортового номера
     if (tok_bnum == nullptr) {
+        // Проверка на наличие, если нет - ошибка в объеме данных
         return ERR_TOK_COUNT;
     }
     if (strlen(tok_bnum) != strlen(bort_prefix) + 4) {
+        // Проверка на размер - длина префикса + 4 цифры
         return ERR_BNUM_VAL;
     }
-    const int bnum_val = parse_with_prefix(bort_prefix, tok_bnum);
+    const int bnum_val = parse_with_prefix(bort_prefix, tok_bnum); // Парсинг числа
     if (bnum_val == -1) {
+        // Обработка ошибки парсинга
         return ERR_BNUM_VAL;
     }
+    // Инициализация поля структуры
     row.bort_number = bnum_val;
 
+    // То же самое для номера рейса
     const char *tok_flight = strtok(nullptr, delim);
     if (tok_flight == nullptr) {
         return ERR_TOK_COUNT;
@@ -201,26 +261,30 @@ inline ErrCode parse_line(char *str, Line &row) {
     }
     row.flight_number = flight_val;
 
-    const char *tok_model = strtok(nullptr, delim);
+    const char *tok_model = strtok(nullptr, delim); // Токен модели ЛА
     if (tok_model == nullptr) {
         return ERR_TOK_COUNT;
     }
     if (strlen(tok_model) >= MAX_NAME_LENGTH) {
+        // Проверка на длину
         return ERR_NAME_LENGTH;
     }
-    strcpy(row.aircraft, tok_model);
+    strcpy(row.aircraft, tok_model); // Инициализация через strcpy
 
-    char *tok_time = strtok(nullptr, delim);
+    char *tok_time = strtok(nullptr, delim); // Токен времени посадки
     if (tok_time == nullptr) {
         return ERR_TOK_COUNT;
     }
     if (strtok(nullptr, delim) != nullptr) {
+        // Если остались еще токены после всех обработок - ошибка в объеме данных (перебор)
         return ERR_TOK_COUNT;
     }
 
+    // Обработка времени
     if (!parse_time(tok_time, row.arrival)) {
         return ERR_TIME_VAL;
     }
 
+    // Если все прошло успешно - возврат GOOD
     return GOOD;
 }
